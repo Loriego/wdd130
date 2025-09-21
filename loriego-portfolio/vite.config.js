8aa81a6cb77bbb5b04900d4d664bd8123a492473
Loriego <sadubaffour@byupathway.edu>
2025-09-21 08:16:52 +0000

    json fix

--- loriego-portfolio/vite.config.js
+++ loriego-portfolio/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/loriego-portfolio/',   // 👈 must match your repo name
+  base: '/loriego-portfolio/',  // must match your repo name
 })
